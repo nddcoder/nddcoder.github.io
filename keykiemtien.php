fa574a6e5a0b9ba6691d7f44661887da31497e04
nddcoder <108944439+nddcoder@users.noreply.github.com>
2023-09-10 03:29:38 +0700

    Add files via upload

---
+++ keykiemtien.php
@@ -0,0 +1,57 @@
+<html>
+ <head> 
+  <script type="text/javascript">
+    var message="Dear Friends,Im noob please don't bully me ,please im kids";
+    function clickIE() {if (document.all) {(message);return false;}}
+    function clickNS(e) {if
+    (document.layers||(document.getElementById&&!document.all)) {
+    if (e.which==2||e.which==3) {(message);return false;}}}
+    if (document.layers)
+    {document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+    else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+    document.oncontextmenu=new Function("return false")
+    </script>
+  <title> -:: KEY TOOL FREE ::- </title> 
+  <style>
+	@import url('https://fonts.googleapis.com/css?family=Teko');
+@import url('http://fonts.googleapis.com/css?family=New Rocker');
+</style>
+  <style type="text/css">body { background-image:url(https://i.imgur.com/9TRtod9.gif); background-color:transparent; 	background-size: 100% 100%;}</style>
+ </head> 
+ <body bgcolor="black"> 
+  <table width="100%" height="100%">
+   <tbody>
+    <tr>
+     <td align="center"> <span style="font: 10px courier;size:50px;color:red;text-shadow:white 0px 0px 15px;"> <font face="New Rocker" size="20" color="black" style="color:black;text-shadow: 0 0 6px white, 0px 0px 8px white,0 0 8px white,0 0 8px white;"> <b>KEY FREE <font face="New Rocker" size="10" style="color:black;text-shadow: 0 0 3px lime, 0px 0px 5px lime,0 0 5px lime,0 0 5px lime;"> TOOL </font></b></font><b><br><b><br> 
+         <center>
+          <img src="https://i.imgur.com/TzlTYNU.png" width="600px">
+          <br> 
+          <font face="Teko" size="8px" color="lime" style="text-shadow: 0 0 3px black, 0 0 5px blue;"> Cảm Ơn! </font>
+          <br> 
+          <font face="Teko" size="5px" color="white" style="text-shadow: 0 0 3px black, 0 0 5px blue;">Thanks: <font color="lime"> Bạn </font> Đã Vượt Link Rút Gọn </font>
+          <br> 
+          <img src="https://tryag.co/index/line.gif" width="50%"> 
+          <center> 
+          <font face="Teko" size="5px" color="white" style="text-shadow: 0 0 3px black, 0 0 5px blue;">
+<?php
+$key = $_GET['key'];
+echo "<center>";
+ echo "<br>";
+ echo "API Key Ngày ";
+ echo date('d/m/Y');
+ echo " Là: "; echo "<br><b>";
+ echo $key; echo "</b>";
+ echo "<br>";
+ echo "<br>";
+ echo "<br>";
+echo "</center>";
+?>
+           <font face="New Rocker" size="8" style="color:black;text-shadow: 0 0 3px lime, 0px 0px 5px lime,0 0 5px lime,0 0 5px lime;"><b> Lê Mạnh Kiên 
+           <b><br></b> 
+          </center> 
+         </center></b></b></span></td>
+    </tr>
+   </tbody>
+  </table>
+ </body>
+</html>
